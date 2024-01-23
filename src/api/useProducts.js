b7c8f5c1c36714baccf72bcc151cc53a05504fe3
Aryan Malla <aryanmalla19@gmail.com>
2024-01-23 08:43:49 +0545

    Meowwwww

--- src/api/useProducts.js
+++ src/api/useProducts.js
@@ -1,21 +1,24 @@
 import axios from './axios';
 import { useEffect, useState } from 'react'
 
-export default function useProducts() {
-    const [data, setData] = useState()
+const useProducts = (params) => {
+    const category = params.category;
+    const page = params.page;
+    const [data, setData] = useState();
     const [loading, setLoading] = useState(false);
-
     useEffect(() => {
         setLoading(true)
-        axios.get("/products").then(res => res.data).then(data => {
+        axios.get("/products?category=" + category + "&page=" + page).then(res => res.data).then(data => {
             setData(data)
         }).finally(() => {
             setLoading(false)
         })
-    }, [])
+    }, [category,page])
 
     return {
         data: data,
         loading: loading,
     }
 }
+
+export default useProducts;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -5,26 +5,37 @@ import useProducts from '../api/useProducts'
 
 const categories = [
   {
-    value: 'children',
+    value: '3',
     label: 'children'
   },
   {
-    value: 'women',
+    value: '1',
     label: 'women'
   },
   {
-    value: 'men',
+    value: '2',
     label: 'men'
   },
   {
-    value: 'preg-women',
+    value: '4',
     label: 'pregnant Women'
   },
 ]
 export default function Shop() {
   const [selectCategories, setSelectCategories] = useState(0);
-  const {data, loading} = useProducts();
-
+  const [page,setPage] = useState(1);
+  const { data, loading } = useProducts({
+    category: selectCategories,
+    page:page
+  });
+  const handleNext = (e)=>{
+    setPage(page+1)
+  }
+  const handlePrev = (e)=>{
+    if(page>1){
+      setPage(page-1)
+    }
+  }
   const handleChange = (e) => {
     if (parseInt(e.target.value) === selectCategories) {
       setSelectCategories(0)
@@ -39,24 +50,29 @@ export default function Shop() {
     <div>
       <Navbar />
       <div className='w-10/12 mx-auto mt-12 mb-24 flex'>
-        <div className='flex flex-col w-4/12'>
+        <div className='flex flex-col w-2/12'>
           <h1 className='font-bold text-xl'>Categories</h1>
-          {categories.map((cat,idx) => (
+          {categories.map((cat) => (
             <div key={cat.value}>
               <label htmlFor={cat.id}>
-                <input type="checkbox" name={cat.value} id={cat.value} onChange={handleChange} value={parseInt(idx+1)} checked={selectCategories === parseInt(idx+1)} />
+                <input type="checkbox" name={cat.value} id={cat.value} onChange={handleChange} value={cat.value} checked={selectCategories === parseInt(cat.value)} />
                 <span>{cat.label}</span>
               </label>
             </div>
           ))}
         </div>
-        <div className='flex w-8/12 flex-wrap justify-between'>
-        {loading ? "Loading......." : null}
+
+        <div className='flex w-9/12 flex-wrap justify-between'>
+          <div className='w-full ml-4 flex'>
+            <button onClick={handlePrev}>Previous</button>
+            <span className='mx-5 text-center bg-blue-400 p-3 text-white text-sm rounded-full h-8 w-8 flex items-center jusitfy-between'>{page}</span>
+            <button onClick={handleNext}>Next</button>
+          </div>
+          {loading ? "Loading......." : null}
           {
             data?.data.map((product) => {
-              if(selectCategories===product.category_id){
-                return (
-                  <div className='w-60 mb-5'>
+              return (
+                <div className='w-60 mb-5'>
                   <div>
                     <img src={product.image} alt='idk' />
                   </div>
@@ -69,32 +85,11 @@ export default function Shop() {
                     <p className='text-base font-bold'>$ {product.price}</p>
                   </div>
                 </div>
-                )
-              }
-              else if(selectCategories===0){
-                return (
-                  <div className='w-60 mb-5'>
-                  <div>
-                    <img src={product.image} alt='idk' />
-                  </div>
-                  <div className='flex mt-2'>
-                    <img src={svg} alt="" />
-                    <p className='ml-1 text-sm mt-1'>{product.rating} rating</p>
-                  </div>
-                  <div>
-                    <p className='text-base font-bold'>{product.name}</p>
-                    <p className='text-base font-bold'>$ {product.price}</p>
-                  </div>
-                </div>
-                )
-              }
-              else{
-                return null;
-              }
+              )
             }
-              
             )
           }
+
         </div>
       </div>
     </div>
